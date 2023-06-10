f19582383568dc6c87612b3b3a09fe57abb1ab8c
Rheznendra Praditya <rheznendra@gmail.com>
2023-06-10 22:42:46 +0800

    fix footer link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer position-relative" data-bg-img="images/bg/05.png">
+<footer class="footer position-relative" data-bg-img="{{ asset('assets/images/bg/05.png') }}">
 	<div class="primary-footer">
 		<div class="container">
 			<div class="row">
@@ -11,34 +11,29 @@
 				</div>
 				<div class="col-lg-4 col-md-6 ps-md-5 mt-5 mt-md-0 footer-list justify-content-between d-flex">
 					<ul class="list-unstyled w-100">
-						<li><a href="about-us.html">About Us</a>
+						<li>
+							<a href="{{ route('home') }}#about">About Us</a>
 						</li>
-						<li><a href="services.html">Service</a>
+						<li>
+							<a href="{{ route('home') }}#service">Service</a>
 						</li>
-						<li><a href="team.html">Team</a>
-						</li>
-						<li><a href="team-single.html">Team Single</a>
-						</li>
-						<li><a href="contact.html">Contact Us</a>
+						<li>
+							<a href="{{ route('home') }}#team">Team</a>
 						</li>
 					</ul>
 					<ul class="list-unstyled w-100">
-						<li><a href="blog-right-sidebar.html">Blog</a>
+						<li>
+							<a href="{{ route('bts') }}">Predict</a>
 						</li>
-						<li><a href="faq.html">Faq</a>
-						</li>
-						<li><a href="error-404.html">Error 404</a>
-						</li>
-						<li><a href="privacy-policy.html">Privacy Policy</a>
-						</li>
-						<li><a href="terms-and-conditions.html">Terms</a>
+						<li>
+							<a href="{{ route('faq') }}">FAQ</a>
 						</li>
 					</ul>
 				</div>
 				<div class="col-lg-4 col-md-12 mt-5 mt-lg-0">
 					<ul class="media-icon list-unstyled">
 						<li>
-							<p class="mb-0">Address: <b>423B, Road Wordwide Country, USA</b>
+							<p class="mb-0">Address: <b>Bung Tomo 1F/3B, Denpasar, Bali, Indonesia</b>
 							</p>
 						</li>
 						<li>Email: <a href="mailto:rheznendra@gmail.com"><b>rheznendra@gmail.com</b></a>
@@ -57,18 +52,6 @@
 					<div class="col-lg-6 col-md-12">
 						<span>Copyright 2023 Petrichor | All Rights Reserved
 						</span>
-					</div>
-					<div class="col-lg-6 col-md-12 text-lg-end mt-3 mt-lg-0">
-						<div class="footer-social">
-							<ul class="list-inline">
-								<li class="me-2"><a href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
-								</li>
-								<li class="me-2"><a href="#"><i class="fab fa-twitter"></i> Twitter</a>
-								</li>
-								<li><a href="#"><i class="fab fa-google-plus-g"></i> Google Plus</a>
-								</li>
-							</ul>
-						</div>
 					</div>
 				</div>
 			</div>
